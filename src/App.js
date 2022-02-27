a3f1ad2d38833c56e41e24214abce555019a867b
downside154 <75163801+downside154@users.noreply.github.com>
2022-02-27 22:46:20 +0900

    feat: useRef to store variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import UserList from './UserList'
 
 function App() {
+  const users = [
+    { 
+        id:1,
+        username: 'tomato',
+        email: 'tomato@tomatoe.com'
+    },
+    { 
+        id:2,
+        username: 'potata',
+        email: 'potatata@pottat.com'
+    },
+    { 
+        id:3,
+        username: '3333',
+        email: '3fdsafdsa3@tomatoe.com'
+    },          
+  ];
+  
+  //useRef can be used to store variables regardless of components being re rendered
+  
+  const nextId = useRef(4);
+  const onCreate = () =>{
+    console.log(nextId.current); //4
+    nextId.current +=1;
+  };
+
   return (
-    <UserList />
+    <UserList users={users} />
 
   )
 };
--- src/UserList.js
+++ src/UserList.js
@@ -10,30 +10,11 @@ function User({ user }) {
 }
 
 
-function UserList(){
-    const users = [
-        { 
-            id:1,
-            username: 'tomato',
-            email: 'tomato@tomatoe.com'
-        },
-        { 
-            id:2,
-            username: 'potata',
-            email: 'potatata@pottat.com'
-        },
-        { 
-            id:3,
-            username: '3333',
-            email: '3fdsafdsa3@tomatoe.com'
-        },          
-    ];
+function UserList({ users }){
+
     return(
         <div>
-            {/* wanted output is: 
-            <User user={users[0]}/>
-            <User user={users[1]}/>
-            <User user={users[2]}/> */}
+
             {
                 users.map(
                     user=>(<User user={user} key={user.id} />)
