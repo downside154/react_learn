633c3b3caafc45e5c314a24747fcc128814fca26
downside154 <75163801+downside154@users.noreply.github.com>
2022-02-27 22:39:14 +0900

    feat: using map over list, key needed when elements are mapped

--- src/UserList.js
+++ src/UserList.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+function User({ user }) {
+    return (
+        <div>
+        <b>{user.username}</b>
+        <span>{user.email}</span>
+        </div>
+    )
+}
 
 
 function UserList(){
@@ -22,18 +30,15 @@ function UserList(){
     ];
     return(
         <div>
-            <div>
-                <b>{users[0].username}</b>
-                <span>{users[0].email}</span>
-            </div>
-            <div>
-                <b>{users[1].username}</b>
-                <span>{users[1].email}</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b>
-                <span>{users[2].email}</span>
-            </div>
+            {/* wanted output is: 
+            <User user={users[0]}/>
+            <User user={users[1]}/>
+            <User user={users[2]}/> */}
+            {
+                users.map(
+                    user=>(<User user={user} key={user.id} />)
+                    )
+            }
         </div>
     )
 }
