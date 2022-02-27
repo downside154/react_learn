230b9dfae1aeac030d407e4e86cc33c1336fbbb8
downside154 <75163801+downside154@users.noreply.github.com>
2022-02-27 22:59:02 +0900

    feat: adding items to an array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList'
+import CreateUser from './CreateUser'
 
 function App() {
-  const users = [
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '', 
+  });
+  
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  
+  const [users, setUsers] = useState([
     { 
         id:1,
         username: 'tomato',
@@ -18,19 +35,35 @@ function App() {
         username: '3333',
         email: '3fdsafdsa3@tomatoe.com'
     },          
-  ];
-  
-  //useRef can be used to store variables regardless of components being re rendered
+  ]);
   
   const nextId = useRef(4);
-  const onCreate = () =>{
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    }
+    setUsers([...users, user]);
+    setInputs({
+      username:'',
+      email:'',
+    })
     console.log(nextId.current); //4
     nextId.current +=1;
   };
 
   return (
+  <>
+    <CreateUser
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+    />
     <UserList users={users} />
-
+  </>
   )
 };
 
