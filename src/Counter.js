b6d06f1587b3b3cb6831ef5ecc468798a0335c2c
downside154 <75163801+downside154@users.noreply.github.com>
2022-02-28 16:54:01 +0900

    feat useReducer hook for counter component

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            throw new Error('Unhandled action');
+    }
+};
 
 function Counter(){
-    const [number, setNumber] = useState(0);
+
+    const [number, dispatch] = useReducer(reducer, 0);
+
+    const onIncrease = () => {
+        dispatch({
+            type: 'INCREMENT'
+        })
+    };
 
     const onDecrease = () => {
-        setNumber(prevNumber => prevNumber - 1)
+        dispatch({
+            type: 'DECREMENT'
+        })
+    };
+    // const [number, setNumber] = useState(0);
+
+    // const onDecrease = () => {
+    //     setNumber(prevNumber => prevNumber - 1)
         // Does the same as onIncrease, but its a updating function for optimization
-    }
-    const onIncrease = () => {
-        setNumber(number + 1);
-    }
+    // }
+    // const onIncrease = () => {
+    //     setNumber(number + 1);
+    // }
     return (
         <div>
             <h1>{number}</h1>
